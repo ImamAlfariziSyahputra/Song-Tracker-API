2fa072129b0b201d66d57973e4b05a0c2da4dd97
ImamAlfariziSyahputra <imam.alfarizi.777@gmail.com>
2021-05-28 06:47:47 +0700

    clean routes, adding validation middleware for register post request

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const config = require('./config/config');
-const { User, sequelize } = require('./models');
+const { sequelize } = require('./models');
 
 const app = express();
 app.use(morgan('combined'));
@@ -16,16 +16,7 @@ app.get('/hello', (req, res) => {
   });
 });
 
-app.post('/register', async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.send(user.toJSON());
-  } catch (err) {
-    res.status(400).send({
-      error: 'This email account is already in use',
-    });
-  };
-});
+require('./routes')(app);
 
 sequelize.sync()
 .then(() => {
